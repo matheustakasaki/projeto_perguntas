4cd15d1474de033bd957cb257bc7fb3720cce064
Matheus Antunes <matheus8antunes@gmail.com>
2024-05-31 21:42:08 -0300

    rdme

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,6 @@ class _PerguntasAppState extends State<PerguntaApp> {
             Text(perguntas[_pergutaSelecionada]),
             ElevatedButton(
                 onPressed: _responder, child: const Text("Resposta 4")),
-            ElevatedButton(
-                onPressed: _responder, child: const Text("Resposta 5")),
           ],
         ),
       ),
